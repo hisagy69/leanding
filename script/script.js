da4eafbda1c003407fd204ef156deada8a0f7c03
Adam <sinegamy11@gmail.com>
2020-08-10 12:18:56 +0700

    lesson28

--- script/script.js
+++ script/script.js
@@ -402,6 +402,104 @@ window.addEventListener('DOMContentLoaded', () => {
 		const statusMessage = document.createElement('div');
 		let keyRequest;
 		statusMessage.style.cssText = 'font-size: 2rem; color: #fff';
+
+		const animate = () => {
+			let start = null,
+					prevItem = null,
+					fantom = null,
+					i = 0;
+
+			statusMessage.innerHTML = `
+				<div class="animate">
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			`;
+
+			const loader = document.querySelector('.animate'),
+						divCollection = loader.querySelectorAll('div');
+
+			statusMessage.querySelector('.animate').style.cssText = `
+				width: 40px;
+				height: 40px;
+				display: flex;
+				flex-wrap: wrap;
+				margin: auto;
+			`;
+			divCollection[2].style.order = 1;
+			[...divCollection].forEach(item => {
+				item.style.cssText = `
+					width: 20px;
+					height: 20px;
+					background: #000;
+					display: inline-block;
+					opacity: 0.1;
+					margin: 0;
+				`;
+			});
+
+			const animateFrame = timestamp => {
+				if (!start) start = timestamp;
+				if (timestamp - start >= 100) {
+					divCollection[i].style.opacity = 1;
+					if (prevItem) prevItem.style.opacity = 0.5;
+					if (fantom) fantom.style.opacity = 0;
+					fantom = prevItem;
+					prevItem = divCollection[i];
+					if (i === 3) {
+						i = 0;
+						start = timestamp;
+						keyRequest = requestAnimationFrame(animateFrame);
+						return;
+					}
+						i++;
+						start = timestamp;
+				}
+				keyRequest = requestAnimationFrame(animateFrame);
+			};
+			keyRequest = requestAnimationFrame(animateFrame);
+		};
+
+		const body = {};
+		const formData = new FormData(form);
+		for (let val of formData.entries()) {
+				body[val[0]] = val[1];
+		}
+
+		const outputData = () => {
+			statusMessage.textContent = successMessage;
+			[...form.elements].forEach(item => {
+				if (item.tagName.toLowerCase() === 'input') {
+					item.value = '';
+				}
+			});
+		};
+		const errorData = error => {
+			statusMessage.textContent = errorMessage;
+			console.error(error);
+		};
+		const postData = body => {
+			return new Promise((resolve, reject) => {
+				const request = new XMLHttpRequest();
+				request.addEventListener('readystatechange', () => {
+					if (request.readyState !== 4) {
+						return;
+					}
+					if (request.status === 200) {
+						cancelAnimationFrame(keyRequest);
+						resolve();
+					} else {
+						cancelAnimationFrame(keyRequest);
+						reject(request.status);
+					}
+				});
+				request.open('POST', './server.php');
+				request.setRequestHeader('Content-Type', 'application/json');
+				request.send(JSON.stringify(body));
+			});
+		};
 		form.addEventListener('submit', event => {
 			for (let item of [...form.elements]) {
 				if (item.classList.contains('error')) {
@@ -410,103 +508,11 @@ window.addEventListener('DOMContentLoaded', () => {
 			}
 			event.preventDefault();
 			form.append(statusMessage);
-
-			const animate = () => {
-				let start = null,
-				 		prevItem = null,
-						fantom = null,
-						i = 0;
-
-				statusMessage.innerHTML = `
-					<div class="animate">
-						<div></div>
-						<div></div>
-						<div></div>
-						<div></div>
-					</div>
-				`;
-
-				const loader = document.querySelector('.animate'),
-							divCollection = loader.querySelectorAll('div');
-
-				statusMessage.querySelector('.animate').style.cssText = `
-					width: 40px;
-					height: 40px;
-					display: flex;
-					flex-wrap: wrap;
-					margin: auto;
-				`;
-				divCollection[2].style.order = 1;
-				[...divCollection].forEach(item => {
-					item.style.cssText = `
-						width: 20px;
-						height: 20px;
-						background: #000;
-						display: inline-block;
-						opacity: 0.1;
-						margin: 0;
-					`;
-				});
-
-				const animateFrame = timestamp => {
-					if (!start) start = timestamp;
-					if (timestamp - start >= 100) {
-						divCollection[i].style.opacity = 1;
-						if (prevItem) prevItem.style.opacity = 0.5;
-						if (fantom) fantom.style.opacity = 0;
-						fantom = prevItem;
-						prevItem = divCollection[i];
-						if (i === 3) {
-							i = 0;
-							start = timestamp;
-							keyRequest = requestAnimationFrame(animateFrame);
-							return;
-						}
-							i++;
-							start = timestamp;
-					}
-					keyRequest = requestAnimationFrame(animateFrame);
-				};
-				keyRequest = requestAnimationFrame(animateFrame);
-			};
-
+			const data = postData(body);
+			data.then(outputData)
+					.catch(errorData);
 			animate();
-			const formData = new FormData(form);
-			let body = {};
-			for (let val of formData.entries()) {
-				body[val[0]] = val[1];
-			}
-			postData(body, () => {
-				statusMessage.textContent = successMessage;
-				[...form.elements].forEach(item => {
-					if (item.tagName.toLowerCase() === 'input') {
-						item.value = '';
-					}
-				});
-			}, error => {
-				statusMessage.textContent = errorMessage;
-				console.error(error);
-			});
 		});
-
-		const postData = (body, outputData, errorData) => {
-			const request = new XMLHttpRequest();
-			request.addEventListener('readystatechange', () => {
-				if (request.readyState !== 4) {
-					return;
-				}
-				if (request.status === 200) {
-					cancelAnimationFrame(keyRequest);
-					outputData();
-				} else {
-					cancelAnimationFrame(keyRequest);
-					errorData(request.status);
-				}
-			});
-			request.open('POST', './server.php');
-			request.setRequestHeader('Content-Type', 'application/json');
-			request.send(JSON.stringify(body));
-		};
 	};
 
 	sendForm('form1');
